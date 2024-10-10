a01e93f4e2fb01d1fced63b431e90f8bcb2bf5a7
xqd7aq <143450447+xqd7aq@users.noreply.github.com>
2024-10-10 16:17:26 -0400

    Finished lab5

---
+++ my-work/lab5/bootstrap_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python3
+sudo apt-get install -y git
+sudo apt-get install -y boto3
+
+#just for trial purposes
+sudo apt-get install -y mysql-server
+sudo systemctl enable mysql
+sudo systemctl start mysql
